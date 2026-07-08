3cfdafafc076a46bf69f82236c2f2f24875a8a20
VyomGarg04 <vyomgarg497@gmail.com>
2026-07-08 14:44:47 +0000

    feat(user): add User model and initial database migration

--- backend/app/database/base.py
+++ backend/app/database/base.py
@@ -1,4 +1,9 @@
 from sqlalchemy.orm import DeclarativeBase
 
+
 class Base(DeclarativeBase):
     pass
+
+
+# this is bellow the class base because first the user imports base then we import user here
+from app.database.models import User
---
+++ backend/app/database/models/user.py
@@ -0,0 +1,24 @@
+from sqlalchemy import String, Boolean
+from sqlalchemy.orm import Mapped, mapped_column
+
+from app.database.base import Base
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+
+    full_name: Mapped[str] = mapped_column(String(100))
+
+    email: Mapped[str] = mapped_column(
+        String(255),
+        unique=True,
+        index=True,
+    )
+
+    hashed_password: Mapped[str] = mapped_column(String(255))
+
+    is_active: Mapped[bool] = mapped_column(default=True)
+
+    is_verified: Mapped[bool] = mapped_column(default=False)
